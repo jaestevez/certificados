b876275825e5a1c187adcc207215d0be6dee87f9
jaestevez <jose_estevez@outlook.com>
2015-01-26 16:59:43 -0500

    git ignored delete

---
+++ config/local.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+
+  /***************************************************************************
+   * Your SSL certificate and key, if you want to be able to serve HTTP      *
+   * responses over https:// and/or use websockets over the wss:// protocol  *
+   * (recommended for HTTP, strongly encouraged for WebSockets)              *
+   *                                                                         *
+   * In this example, we'll assume you created a folder in your project,     *
+   * `config/ssl` and dumped your certificate/key files there:               *
+   ***************************************************************************/
+
+  // ssl: {
+  //   ca: require('fs').readFileSync(__dirname + './ssl/my_apps_ssl_gd_bundle.crt'),
+  //   key: require('fs').readFileSync(__dirname + './ssl/my_apps_ssl.key'),
+  //   cert: require('fs').readFileSync(__dirname + './ssl/my_apps_ssl.crt')
+  // },
+
+  port: process.env.PORT || 5000,
+
+  environment: process.env.NODE_ENV || 'development'
+
+};
