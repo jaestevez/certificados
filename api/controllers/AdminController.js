f39d09522c00b0167c365741c1bf4115a6e8166d
Jose Estevez <jose_estevez@outlook.com>
2015-06-07 16:22:03 -0500

    adding API tetser

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -5,7 +5,7 @@ module.exports = {
 	all_content: function(req, res){
 		sails.models.files_pdf_format.query('SELECT cc,name,time_live,date_file,validated,name_file FROM files_pdf_format f,person p where f.id_person = p.cc ',function(err, results) {
 			if (err) return res.serverError(err);
-			//res.send(results.rows);
+			console.log(results.rows);
 			res.view('admin', {formatos:results.rows});
 		});
 	},
