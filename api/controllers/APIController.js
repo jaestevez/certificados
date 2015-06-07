e7cd87e581c3142c71297438ba59e22d8ad7d591
Jose Estevez <jose_estevez@outlook.com>
2015-06-07 16:34:39 -0500

    adding API v0.01

--- api/controllers/APIController.js
+++ api/controllers/APIController.js
@@ -1,6 +1,9 @@
 module.exports = {
   registros: function (req, res) {
-  	res.send("{res:'registros'}");
+  	sails.models.files_pdf_format.query('SELECT cc,name,time_live,date_file,validated,name_file FROM files_pdf_format f,person p where f.id_person = p.cc ',function(err, results) {
+		if (err) return res.serverError(err);
+			res.send(results.rows);
+		});
   },
   cambioEstado: function (req, res){
   	res.send("{res:'cambio estado'}");
