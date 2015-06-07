d54b0cec7b0e12e38aba0a16a3e5a3f49aca0811
Jose Estevez <jose_estevez@outlook.com>
2015-06-07 13:14:11 -0500

    alpha interface admin user v0.07

--- api/controllers/AdminUsersController.js
+++ api/controllers/AdminUsersController.js
@@ -1,24 +1,24 @@
 module.exports = {
   adminIU: function (req, res) {
-  	var request_view;
+  	var request_view ="";
   	function queries_model(callback){
       sails.models.user.find()
         .exec(function (err, allTheStuff) {
-          /*text_request += allTheStuff.length + '<br><br><hr>';
+          text_request = "
+          	<div class="form-group"> ";
           var req_view = '';
           for (var i = 0; i < allTheStuff.length; i++) {
-            req_view += "nombre: " + allTheStuff[i].name + 'correo electronico: ' + allTheStuff[i].email;
-            req_view += ' contraseña: ' + allTheStuff[i].password  + "<br><hr>";
+            req_view += "<label> nombre: " + allTheStuff[i].name + '<label> <h3> correo electronico: ' + allTheStuff[i].email
+            + "</h3> <hr> ";
           }
-          text_request += req_view;*/
-          request_view = allTheStuff;
+          request_view += req_view + " <div>";
           console.log(request_view);
           callback(request_view);
         });
     }
     queries_model(function(){
       //return res.send(text_request);
-      res.view('admin-interface', {users_data:request_view});
+      res.view('admin-interface', {data_embedded:request_view});
     });
   	//res.view('homepage');
   }
