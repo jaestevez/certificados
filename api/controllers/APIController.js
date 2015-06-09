0d4701e5a22f44cc13ec616071e5650447c0f9ed
Jose Estevez <jose_estevez@outlook.com>
2015-06-09 16:18:12 -0500

    API modificacion 06

--- api/controllers/APIController.js
+++ api/controllers/APIController.js
@@ -2,7 +2,7 @@ module.exports = {
   registros: function (req, res) {
   	sails.models.files_pdf_format.query('SELECT cc,name,time_live,date_file,validated,name_file FROM files_pdf_format f,person p where f.id_person = p.cc ',function(err, results) {
 		if (err) return res.serverError(err);
-			var data = results.rows;
+			var data = JSON.stringify(results.rows);
 			console.log(typeof(data));
 			res.send(data);
 		});
