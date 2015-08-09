a96d5a1ed009427b511903bba3048b95d9b6f587
Jose Estevez <jose_estevez@outlook.com>
2015-08-09 18:22:45 -0500

    build raw.html

--- api/controllers/APIController.js
+++ api/controllers/APIController.js
@@ -1,9 +1,24 @@
 module.exports = {
   registros: function (req, res) {
+      console.log(req.url);
+      console.log(req.query);
+      if(!req.query.start || !req.query.end){
   	sails.models.files_pdf_format.query('SELECT cc,name,time_live,date_file,validated,name_file FROM files_pdf_format f,person p where f.id_person = p.cc ',function(err, results) {
 		if (err) return res.serverError(err);
+            console.log('numb 1');
 			res.send(results.rows);
 		});
+      }else{
+          sails.models.files_pdf_format.query('SELECT cc,name,time_live,date_file,validated,name_file FROM files_pdf_format f,person p where f.id_person = p.cc ',function(err, results) {
+		if (err) return res.serverError(err);
+              rowsf = results.rows;
+              var results_mod = [];
+              for(var i=parseInt(req.query.start);i<=parseInt(req.query.end);i++)
+              results_mod.push(rowsf[i]);
+              console.log('numb 2');
+			res.send(results_mod);
+		});
+      }
   },
   cambioEstado: function (req, res){
   	res.send("{res:'cambio estado'}");
